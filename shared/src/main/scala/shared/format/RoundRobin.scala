bb065543a85df635e577464619816d1c14eed616
jorolicht <31469139+jorolicht@users.noreply.github.com>
2026-06-22 18:49:35 +0200

    Update Round Robin draw help and group header name to Gruppe Jeder-gegen-Jeden

--- shared/src/main/scala/shared/format/RoundRobin.scala
+++ shared/src/main/scala/shared/format/RoundRobin.scala
@@ -14,7 +14,7 @@ object RoundRobin {
    * Initializes a RoundRobinStage stage data structure with a single group.
    */
   def draw(stage: Stage, coTyp: CompTyp, cfg: StageConfig, selectedPants: Seq[Pant], drawOption: DrawOption = DrawOption.Unknown): StageData.RoundRobinStage = {
-    val g = Group(1, selectedPants.length, 1, "Gruppe 1", stage.noWinSets)
+    val g = Group(1, selectedPants.length, 1, "Gruppe Jeder-gegen-Jeden", stage.noWinSets)
     selectedPants.zipWithIndex.foreach { case (p, i) => g.pants(i) = p }
     val rrStage: StageData.RoundRobinStage = StageData.RoundRobinStage(g)
 
