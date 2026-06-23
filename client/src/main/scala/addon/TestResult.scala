03299a98cdb8c5273fd211b98d32a335f99c4674
jorolicht <31469139+jorolicht@users.noreply.github.com>
2026-06-23 13:18:15 +0200

    Update TestResult to enter scores after 1s and recursively process batches until all games are finished

--- client/src/main/scala/addon/TestResult.scala
+++ client/src/main/scala/addon/TestResult.scala
@@ -21,7 +21,8 @@ object TestResult extends JsWrapper:
         Future(Left(AppError("unknown test number")))
 
   /**
-   * Automatically schedules demo table assignments and random results for up to 3 ready matches.
+   * Automatically schedules demo table assignments and random results for up to 3 ready matches,
+   * then recursively checks and completes new playable matches until the entire stage is finished.
    *
    * @param group The test group name.
    * @param number The test number.
@@ -41,69 +42,97 @@ object TestResult extends JsWrapper:
         val errMsg = "Fehler: Keine Stage ausgewählt!"
         addOutput(errMsg)
         Future(Left(AppError(errMsg)))
-      case Some(stage) =>
-        val allMatches = stage.matches.toSeq
-        val readyMatches = allMatches.filter(m => m.status == MEntry.MS_READY && m.result.trim.isEmpty)
+      case Some(initialStage) =>
         
-        if (readyMatches.isEmpty) {
-          addOutput("Keine spielbereiten Spiele ohne Ergebnisse gefunden.")
-          Future(Right("No ready matches found"))
-        } else {
-          val matchesToRun = readyMatches.take(3)
-          addOutput(s"Simuliere Spielstart für ${matchesToRun.length} Spiele...")
-
-          matchesToRun.foreach { m =>
-            val gameNo = m.gameNo
-            val tableVal = (1 + scala.util.Random.nextInt(12)).toString
-            
-            // 1. Assign table in DOM and dispatch change
-            val tableInput = dom.document.getElementById(s"table_$gameNo").asInstanceOf[dom.html.Input]
-            if (tableInput != null) {
-              tableInput.value = tableVal
-              tableInput.dispatchEvent(new dom.Event("change"))
-              addOutput(s"Spiel $gameNo: Tisch $tableVal zugewiesen.")
-            } else {
-              addOutput(s"Spiel $gameNo: Tisch-Input im DOM nicht gefunden.")
-            }
-
-            // 2. Schedule result entry after 3 seconds
-            dom.window.setTimeout(() => {
-              val winSets = stage.noWinSets
-              var aWins = 0
-              var bWins = 0
-              var setIdx = 1
+        // Define recursive batch execution loop
+        def runNextBatch(): Unit = {
+          Global.currentSelection.stage match {
+            case None =>
+              addOutput("Automatisierung gestoppt: Keine Stage ausgewählt.")
+            case Some(stage) =>
+              val allMatches = stage.matches.toSeq
+              val unfinishedMatches = allMatches.filter(m => !m.finished && m.result.trim.isEmpty && m.status != MEntry.MS_FIX)
               
-              addOutput(s"Spiel $gameNo: Trage zufälliges Ergebnis ein...")
-              
-              while (aWins < winSets && bWins < winSets) {
-                val aWinsSet = scala.util.Random.nextBoolean()
-                val score = if (aWinsSet) {
-                  aWins += 1
-                  (3 + scala.util.Random.nextInt(6)).toString
-                } else {
-                  bWins += 1
-                  s"-${3 + scala.util.Random.nextInt(6)}"
-                }
-                
-                val setInput = dom.document.getElementById(s"input_${gameNo}_$setIdx").asInstanceOf[dom.html.Input]
-                if (setInput != null) {
-                  setInput.value = score
-                  setInput.dispatchEvent(new dom.Event("input"))
-                }
-                setIdx += 1
-              }
-
-              // Click save button (green checkmark)
-              val saveBtn = dom.document.getElementById(s"${StageInput.SaveMatchBtn.id}-$gameNo").asInstanceOf[dom.html.Button]
-              if (saveBtn != null) {
-                saveBtn.click()
-                addOutput(s"Spiel $gameNo: Ergebnis gespeichert.")
+              if (unfinishedMatches.isEmpty) {
+                addOutput("Alle Spiele dieser Stage sind beendet!")
               } else {
-                addOutput(s"Spiel $gameNo: Save-Button nicht gefunden.")
-              }
-            }, 3000)
-          }
+                val readyMatches = allMatches.filter(m => m.status == MEntry.MS_READY && m.result.trim.isEmpty)
+                
+                if (readyMatches.isEmpty) {
+                  // Check if there are matches currently running
+                  val runningMatches = allMatches.filter(m => m.status == MEntry.MS_RUN && m.result.trim.isEmpty)
+                  if (runningMatches.nonEmpty) {
+                    addOutput("Warte auf laufende Spiele...")
+                    // Check again in 1 second
+                    dom.window.setTimeout(() => runNextBatch(), 1000)
+                  } else {
+                    addOutput("Automatisierung beendet: Keine weiteren spielbereiten Spiele vorhanden (weitere Runden ggf. blockiert oder beendet).")
+                  }
+                } else {
+                  val matchesToRun = readyMatches.take(3)
+                  addOutput(s"Starte nächste Charge: ${matchesToRun.length} spielbereite(s) Spiel(e)...")
 
-          Future(Right(s"Demo results generation scheduled for ${matchesToRun.length} matches"))
+                  var completedCount = 0
+
+                  matchesToRun.foreach { m =>
+                    val gameNo = m.gameNo
+                    val tableVal = (1 + scala.util.Random.nextInt(12)).toString
+                    
+                    // 1. Assign table in DOM and dispatch change
+                    val tableInput = dom.document.getElementById(s"table_$gameNo").asInstanceOf[dom.html.Input]
+                    if (tableInput != null) {
+                      tableInput.value = tableVal
+                      tableInput.dispatchEvent(new dom.Event("change"))
+                      addOutput(s"Spiel $gameNo: Tisch $tableVal zugewiesen.")
+                    }
+
+                    // 2. Schedule result entry after 1 second (1000 ms)
+                    dom.window.setTimeout(() => {
+                      val winSets = stage.noWinSets
+                      var aWins = 0
+                      var bWins = 0
+                      var setIdx = 1
+                      
+                      while (aWins < winSets && bWins < winSets) {
+                        val aWinsSet = scala.util.Random.nextBoolean()
+                        val score = if (aWinsSet) {
+                          aWins += 1
+                          (3 + scala.util.Random.nextInt(6)).toString
+                        } else {
+                          bWins += 1
+                          s"-${3 + scala.util.Random.nextInt(6)}"
+                        }
+                        
+                        val setInput = dom.document.getElementById(s"input_${gameNo}_$setIdx").asInstanceOf[dom.html.Input]
+                        if (setInput != null) {
+                          setInput.value = score
+                          setInput.dispatchEvent(new dom.Event("input"))
+                        }
+                        setIdx += 1
+                      }
+
+                      // Click save button (green checkmark)
+                      val saveBtn = dom.document.getElementById(s"${StageInput.SaveMatchBtn.id}-$gameNo").asInstanceOf[dom.html.Button]
+                      if (saveBtn != null) {
+                        saveBtn.click()
+                        addOutput(s"Spiel $gameNo: Ergebnis gespeichert.")
+                      }
+
+                      completedCount += 1
+                      if (completedCount == matchesToRun.length) {
+                        // Wait 800ms for AJAX and local selection updates to settle, then run next batch
+                        dom.window.setTimeout(() => {
+                          runNextBatch()
+                        }, 800)
+                      }
+                    }, 1000)
+                  }
+                }
+              }
+          }
         }
+
+        // Start the recursive waterfall
+        runNextBatch()
+        Future(Right("Automatischer Durchlauf gestartet"))
     }
