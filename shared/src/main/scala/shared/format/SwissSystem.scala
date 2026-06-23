b36288dfad5e95420070ca07c6d0be95f6510f5f
jorolicht <31469139+jorolicht@users.noreply.github.com>
2026-06-23 20:36:14 +0200

    Sort Swiss System players by TTR descending, set seeding pos in place._1, and render this seeding index on player badges even after swaps

--- shared/src/main/scala/shared/format/SwissSystem.scala
+++ shared/src/main/scala/shared/format/SwissSystem.scala
@@ -12,13 +12,18 @@ object SwissSystem {
    * Initializes a SwissStage stage data structure with a single group.
    */
   def draw(selectedPants: Seq[Pant], noWinSets: Int, drawOption: DrawOption = DrawOption.Unknown): StageData.SwissStage = {
-    val updatedPants = if (selectedPants.length % 2 == 1) {
-      selectedPants :+ Pant(id = SNO.bye(selectedPants.length), name = "")
+    val sortedActivePants = selectedPants.sortBy(-_.rating)
+    val updatedPants = if (sortedActivePants.length % 2 == 1) {
+      sortedActivePants :+ Pant(id = SNO.bye(sortedActivePants.length), name = "")
     } else {
-      selectedPants
+      sortedActivePants
     }
     val g = Group(1, updatedPants.length, 1, "Schweizer System", noWinSets)
-    updatedPants.zipWithIndex.foreach { case (p, i) => g.pants(i) = p }
+    updatedPants.zipWithIndex.foreach { case (p, i) =>
+      val pCopy = p.copy()
+      pCopy.place = (i + 1, 0)
+      g.pants(i) = pCopy
+    }
     StageData.SwissStage(g)
   }
 }
